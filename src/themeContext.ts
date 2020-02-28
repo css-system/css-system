85b3c2aba6efdb64e8df1146d5bcc5a578840eb9
Benjamin Millagou <benjamin@jfg-networks.net>
2020-02-28 14:49:38 +0100

    fix themeContext export

---
+++ src/themeContext.ts
@@ -0,0 +1,11 @@
+import {createContext} from "react"
+
+export const ThemeContext = createContext({
+  breakpoints: {
+    s: "40em",
+    m: "52em",
+    l: "64em",
+  },
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 72],
+})
